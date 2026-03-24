632ef1f1aa170c684b2b181625a6db58bf81eff9
Nawaaz Mohammed <md.nawaaz0786@gmail.com>
2026-03-24 16:54:31 +0530

    fix: added map-migrated tag

--- s3/locals.tf
+++ s3/locals.tf
@@ -1,11 +1,12 @@
 locals {
   common_tags = merge(var.tags, {
-    Environment = var.environment
-    Application = var.application
-    CostCentre  = var.cost_centre
-    Owner       = var.owner
-    ManagedBy   = var.managed_by
-    Module      = var.module
+    Environment   = var.environment
+    Application   = var.application
+    CostCentre    = var.cost_centre
+    Owner         = var.owner
+    ManagedBy     = var.managed_by
+    Module        = var.module
+    map-migrated  = "migFM25HRY5PO"
   })
 }
 
--- vpc/locals.tf
+++ vpc/locals.tf
@@ -2,12 +2,13 @@ locals {
   vpc_name = "vpc-${var.environment}"
 
   common_tags = merge(var.tags, {
-    Environment = var.environment
-    Application = var.application
-    CostCentre  = var.cost_centre
-    Owner       = var.owner
-    ManagedBy   = var.managed_by
-    Module      = var.module
+    Environment   = var.environment
+    Application   = var.application
+    CostCentre    = var.cost_centre
+    Owner         = var.owner
+    ManagedBy     = var.managed_by
+    Module        = var.module
+    map-migrated  = "migFM25HRY5PO"
   })
 
   nat_gateway_count = var.enable_nat_gateway ? (
