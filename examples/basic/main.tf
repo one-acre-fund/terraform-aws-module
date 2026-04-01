01dd7e5ec5c2565ba44b8758c8e3c4ba35800071
Soumya <soumya_medepalli@epam.com>
2026-04-01 15:23:48 +0530

    fix: Modified examples

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -99,7 +99,7 @@ module "database" {
   db_name              = var.db_engine == "postgres" ? var.db_name : null
   db_identifier        = "${var.application}-${var.environment}-db"
   db_subnet_group_name = "${var.application}-${var.environment}-subnet-grp"
-  subnet_ids           = module.vpc.private_subnet_ids
+  # subnet_ids           = module.vpc.private_subnet_ids
 
   engine         = var.db_engine
   engine_version = var.db_engine_version
