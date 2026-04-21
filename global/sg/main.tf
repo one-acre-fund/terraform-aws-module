e913c4a73b1d056b9fc8d822cdfbe74d17443d54
Nawaaz Mohammed <md.nawaaz0786@gmail.com>
2026-04-21 11:26:19 +0530

    fix: Added Sg dynamic blocks for ingress and egrees

--- global/sg/main.tf
+++ global/sg/main.tf
@@ -9,30 +9,30 @@ resource "aws_security_group" "this" {
   dynamic "ingress" {
     for_each = var.ingress_rules
     content {
-      description              = lookup(ingress.value, "description", null)
-      from_port                = ingress.value.from_port
-      to_port                  = ingress.value.to_port
-      protocol                 = ingress.value.protocol
-      cidr_blocks              = length(coalesce(lookup(ingress.value, "security_groups", []), [])) > 0 ? null : coalesce(lookup(ingress.value, "cidr_blocks", []), [])
-      ipv6_cidr_blocks         = []
-      prefix_list_ids          = []
-      security_groups          = length(coalesce(lookup(ingress.value, "security_groups", []), [])) > 0 ? ingress.value.security_groups : []
-      self                     = false
+      description      = lookup(ingress.value, "description", null)
+      from_port        = ingress.value.from_port
+      to_port          = ingress.value.to_port
+      protocol         = ingress.value.protocol
+      cidr_blocks      = length(lookup(ingress.value, "security_groups", [])) > 0 ? [] : lookup(ingress.value, "cidr_blocks", [])
+      security_groups  = lookup(ingress.value, "security_groups", [])
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      self             = false
     }
   }
 
   dynamic "egress" {
     for_each = var.egress_rules
     content {
-      description              = lookup(egress.value, "description", null)
-      from_port                = egress.value.from_port
-      to_port                  = egress.value.to_port
-      protocol                 = egress.value.protocol
-      cidr_blocks              = length(coalesce(lookup(egress.value, "security_groups", []), [])) > 0 ? null : coalesce(lookup(egress.value, "cidr_blocks", []), [])
-      ipv6_cidr_blocks         = []
-      prefix_list_ids          = []
-      security_groups          = length(coalesce(lookup(egress.value, "security_groups", []), [])) > 0 ? egress.value.security_groups : []
-      self                     = false
+      description      = lookup(egress.value, "description", null)
+      from_port        = egress.value.from_port
+      to_port          = egress.value.to_port
+      protocol         = egress.value.protocol
+      cidr_blocks      = length(lookup(egress.value, "security_groups", [])) > 0 ? [] : lookup(egress.value, "cidr_blocks", [])
+      security_groups  = lookup(egress.value, "security_groups", [])
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      self             = false
     }
   }
 
